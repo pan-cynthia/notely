ff1afb5e9779e0398c71a6b987c06b5aab6e4f8c
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-02 13:29:42 -0700

    add page imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 
 const App = () => {
