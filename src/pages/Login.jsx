1ec291efbd72c0b59ade8d8db46ec0cbeb9edf83
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-03 20:40:43 -0700

    show/hide password on icon click

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 import { Link } from 'react-router-dom'
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6'
 
 const Login = () => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
   return (
     <>
       <NavBar/>
@@ -12,8 +18,8 @@ const Login = () => {
           <form onSubmit={() => {}}>
             <input className="w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none" type="email" placeholder="Email"/>
             <div className="flex items-center justify-between w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none">
-              <input className="flex-3/4 mr-2 outline-none" type="password" placeholder="Password"/>
-              <FaRegEye className="text-blue-500 cursor-pointer" size={20}/>
+              <input className="flex-3/4 mr-2 outline-none" type={isShowPassword ? "text" : "password"} placeholder={"Password"}/>
+              {isShowPassword ? <FaRegEye className="text-blue-500 cursor-pointer" size={20} onClick={toggleShowPassword}/> : <FaRegEyeSlash className="text-blue-500 cursor-pointer" size={20} onClick={toggleShowPassword}/>}
             </div>
             <button className="w-full p-2 my-1 text-sm font-bold text-white bg-blue-500 rounded hover:bg-blue-600" type="submit">Login</button>
             <p className="text-center mt-4 text-sm font-medium">
