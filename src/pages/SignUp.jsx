6a6929389aa5e2f70f473dddf908612db5197875
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-04 13:45:45 -0700

    validate entered sign up info

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Password from '../components/Password'
 import { useState } from 'react'
+import { validateEmail } from '../hooks/validateEmail'
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -9,12 +10,29 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!name) {
+      setError("Please enter your name.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter a password.");
+      return;
+    }
+    setError("");
+  }
+
   return (
     <>
       <NavBar/>
       <div className="flex justify-center items-center mt-30">
         <div className="w-96 px-7 py-10 bg-white rounded-xl shadow-2xl">
-          <form>
+          <form onSubmit={handleSignUp} noValidate>
             <input className="w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"></input>
             <input className="w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
             <Password value={password} onChange={(e) => setPassword(e.target.value)} placeholder={"Password"}/>
