39315bbf5272b47720ce0a5cda56edcaf8603754
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-03 21:22:19 -0700

    validate entered email address

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,22 +7,40 @@ const Login = () => {
   const [isShowPassword, setIsShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const toggleShowPassword = () => {
     setIsShowPassword(!isShowPassword);
   };
+
+  const handleLogin = async (e) => {
+    console.log("entered handle login");
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+  }
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regex: ^ = not, \s = whitespace char, @ = at symbol, combined = any char that is not a whitespace or at symbol
+    return regex.test(email);
+  }
 
   return (
     <>
       <NavBar/>
       <div className='flex justify-center items-center mt-30'>
         <div className="w-96 px-7 py-10 bg-white rounded-xl shadow-2xl">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <input className="w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
             <div className="flex items-center justify-between w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none">
               <input className="flex-3/4 mr-2 outline-none" type={isShowPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} placeholder={"Password"}/>
               {isShowPassword ? <FaRegEye className="text-blue-500 cursor-pointer" size={20} onClick={toggleShowPassword}/> : <FaRegEyeSlash className="text-blue-500 cursor-pointer" size={20} onClick={toggleShowPassword}/>}
             </div>
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
             <button className="w-full p-2 my-1 text-sm font-bold text-white bg-blue-500 rounded hover:bg-blue-600" type="submit">Login</button>
             <p className="text-center mt-4 text-sm font-medium">
               Not registered yet?{" "}
