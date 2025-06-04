300818405f6ad7e435c25762f432a30561a6ceb9
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-04 15:34:40 -0700

    display user's initials as their profile image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,12 @@
+import { getInitials } from "../hooks/useValidate"
+
 const Profile = () => {
   return (
     <>
       <div className="flex items-center gap-3">
-        <div className="flex justify-center items-center w-12 h-12 rounded-full font-medium bg-slate-100">TU</div>
+        <div className="flex justify-center items-center w-12 h-12 rounded-full font-medium bg-slate-100">{getInitials("Test User")}</div>
         <div className="flex flex-col items-center">
-          <p className="text-sm font-medium">Name</p>
+          <p className="text-sm font-medium">Test User</p>
           <button className="text-sm underline cursor-pointer">Logout</button>
         </div>
       </div>
