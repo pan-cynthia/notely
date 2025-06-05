2e0a349d96b47bf4463dc3637910ebb7b018fa22
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-05 09:39:09 -0700

    add close button that clears the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
 const SearchBar = ({value, onChange, handleSearch, clearSearch}) => {
 
@@ -6,6 +7,7 @@ const SearchBar = ({value, onChange, handleSearch, clearSearch}) => {
     <>
       <div className="flex justify-between items-center w-96 px-4 bg-slate-100 rounded-md">
         <input className="w-full text-xs py-[11px] mr-2 outline-none" type="text" placeholder="Search Notes" value={value} onChange={onChange}/>
+        {value && <IoMdClose className="mr-2 text-xl text-slate-500 cursor-pointer hover:text-black" onClick={clearSearch}/>}
         <FaMagnifyingGlass className="text-slate-500 cursor-pointer hover:text-black" onClick={handleSearch}/>
       </div>
     </>
