420c1e70f9e167c9a630a133ca7ea58803597ab0
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-04 19:26:58 -0700

    redirect to login page after user logs out

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,20 @@
 import { getInitials } from "../hooks/useValidate"
+import { useNavigate } from "react-router-dom"
 
 const Profile = () => {
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    navigate('/login');
+  }
+
   return (
     <>
       <div className="flex items-center gap-3">
-        <div className="flex justify-center items-center w-12 h-12 rounded-full font-medium bg-slate-100">{getInitials("Test User")}</div>
+        <div className="flex justify-center items-center w-12 h-12 rounded-full font-me dium bg-slate-100">{getInitials("Test User")}</div>
         <div className="flex flex-col items-center">
           <p className="text-sm font-medium">Test User</p>
-          <button className="text-sm underline cursor-pointer">Logout</button>
+          <button className="text-sm underline cursor-pointer" onClick={handleLogout}>Logout</button>
         </div>
       </div>
     </>
