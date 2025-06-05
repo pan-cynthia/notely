965f56e77bd8a3945feb93d87999feb93569c67d
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-05 09:38:06 -0700

    use state to store search query and initialize functions to handle searches

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import Profile from '../components/Profile.jsx'
 import SearchBar from '../components/SearchBar.jsx'
 
 const NavBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    console.log(searchQuery);
+    clearSearch();
+  }
+
+  const clearSearch = () => {
+    setSearchQuery("");
+  }
+
   return (
     <div className="flex items-center px-6 py-2 bg-white drop-shadow">
       <div className="flex-1/3">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
       </div>
       <div className="flex flex-1/3 justify-center">
-        <SearchBar/>
+        <SearchBar value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} handleSearch={handleSearch} clearSearch={clearSearch}/>
       </div>
       <div className="flex flex-1/3 justify-end">
         <Profile/>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
-const SearchBar = () => {
+const SearchBar = ({value, onChange, handleSearch, clearSearch}) => {
 
   return (
     <>
       <div className="flex justify-between items-center w-96 px-4 bg-slate-100 rounded-md">
-        <input className="w-full text-xs py-[11px] mr-2 outline-none" type="text" placeholder="Search Notes"/>
-        <FaMagnifyingGlass className="text-slate-500 cursor-pointer hover:text-black"/>
+        <input className="w-full text-xs py-[11px] mr-2 outline-none" type="text" placeholder="Search Notes" value={value} onChange={onChange}/>
+        <FaMagnifyingGlass className="text-slate-500 cursor-pointer hover:text-black" onClick={handleSearch}/>
       </div>
     </>
   )
