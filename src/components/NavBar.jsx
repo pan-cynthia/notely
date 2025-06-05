4f4c4a3feed33d95fe656d9bdbcf0fa4fd05e57e
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-05 09:26:27 -0700

    center search bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,16 @@ import SearchBar from '../components/SearchBar.jsx'
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between px-6 py-2 bg-white drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-      <SearchBar/>
-      <Profile/>
+    <div className="flex items-center px-6 py-2 bg-white drop-shadow">
+      <div className="flex-1/3">
+        <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+      </div>
+      <div className="flex flex-1/3 justify-center">
+        <SearchBar/>
+      </div>
+      <div className="flex flex-1/3 justify-end">
+        <Profile/>
+      </div>
     </div>
   )
 }
