db33e5ef537723a834b204f4cfec0ab58fd8968b
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-11 15:37:41 -0700

    add authentication function to check tokens

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized, no token provided." });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403),json({ error: "Forbidden, invalid token." });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+export default authenticateToken;
