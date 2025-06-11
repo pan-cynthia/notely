2ccc044c33766aeb8747c487ed79fe716a85bd92
Cynthia Pan <cynthiapan11@gmail.com>
2025-06-11 15:52:04 -0700

    add CORS

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,16 @@ mongoose
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(3000);
+
+export default app;
