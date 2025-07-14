c5936b7209ee123a4648ccd1efc8f8e7d1985af2
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-14 08:44:48 -0700

    don't display search bar + profile on login/signup pages

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Profile from '../components/Profile.jsx';
 import SearchBar from '../components/SearchBar.jsx';
 
@@ -14,17 +15,24 @@ const NavBar = () => {
     setSearchQuery("");
   }
 
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
     <div className="flex items-center px-6 py-2 bg-white drop-shadow">
       <div className="flex-1/3">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
       </div>
-      <div className="flex flex-1/3 justify-center">
-        <SearchBar value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} handleSearch={handleSearch} clearSearch={clearSearch}/>
-      </div>
-      <div className="flex flex-1/3 justify-end">
-        <Profile/>
-      </div>
+      {isHomePage && (
+        <>
+          <div className="flex flex-1/3 justify-center">
+            <SearchBar value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} handleSearch={handleSearch} clearSearch={clearSearch}/>
+          </div>
+          <div className="flex flex-1/3 justify-end">
+            <Profile/>
+          </div>
+        </>
+      )}
     </div>
   )
 }
