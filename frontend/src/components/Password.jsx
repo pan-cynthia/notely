50adf77fd4db9bfd952585f54e2153ad7b6c2088
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-14 09:08:37 -0700

    display error message on form submit button not password toggle button

--- frontend/src/components/Password.jsx
+++ frontend/src/components/Password.jsx
@@ -12,8 +12,8 @@ const Password = ({value, onChange, placeholder}) => {
      <div className="flex items-center justify-between w-full px-5 py-3 mb-4 text-sm rounded shadow outline-none">
         <input className="flex-3/4 mr-2 outline-none" type={isShowPassword ? "text" : "password"} value={value} onChange={onChange} placeholder={placeholder || "Password"}/>
         {isShowPassword ? 
-        <button className="cursor-pointer" onClick={toggleShowPassword}><FaRegEye className="text-blue-500" size={20}/></button> :
-        <button className="cursor-pointer" onClick={toggleShowPassword}><FaRegEyeSlash className="text-blue-500" size={20}/></button>}
+        <button className="cursor-pointer" onClick={toggleShowPassword} type="button"><FaRegEye className="text-blue-500" size={20}/></button> :
+        <button className="cursor-pointer" onClick={toggleShowPassword} type="button"><FaRegEyeSlash className="text-blue-500" size={20}/></button>}
       </div>
   )
 }
