0398ed819d9208ff12cd6f0c1cdd7aaea29b6973
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-17 13:33:25 -0700

    get and display all of user's notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,24 +19,41 @@ const Home = () => {
   }
 
   const [userInfo, setUserInfo] = useState(null);
+  const [allNotes, setAllNotes] = useState([]);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axiosInstance.get('/auth/get-user');
-        if (response.data && response.data.user) {
-          setUserInfo(response.data.user);
-        }
-      } catch (error) {
-        if (error.response && error.response.status === 401) { // user not found, log out
-          localStorage.clear();
-          navigate('/login');
-        }
+  // get logged in user
+  const fetchUser = async () => {
+    try {
+      const response = await axiosInstance.get('/auth/get-user');
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) { // user not found, log out
+        localStorage.clear();
+        navigate('/login');
       }
     }
+  }
+
+  // get all of user's notes
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get('/notes/get-all-notes');
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.");
+    }
+  }
+
+  useEffect(() => {
     fetchUser();
+    getAllNotes();
+    return () => {};
   }, []);
 
   return (
@@ -44,46 +61,19 @@ const Home = () => {
       <NavBar userInfo={userInfo}/>
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-10">
-          <NoteCard
-            title="Organize Home Office"
-            date="Jun 5, 2025"
-            content="My home office has become a cluttered mess, making it difficult to find things."
-            tags="#Organization"
-            isPinned={true}
-            pinNote={() => {}}
-            editNote={() => {}}
-            deleteNote={() => {}}
-          />
-          <NoteCard
-            title="Start Learning Guitar"
-            date="Jun 5, 2025"
-            content="Learning to play the guitar has always been a dream of mine."
-            tags="#Guitar"
-            isPinned={false}
-            pinNote={() => {}}
-            editNote={() => {}}
-            deleteNote={() => {}}
-          />
-          <NoteCard
-            title="Try New Recipe"
-            date="Jun 5, 2025"
-            content="Cooking is one of my favorite hobbies, and I love experimenting in the kitchen."
-            tags="#Cooking"
-            isPinned={false}
-            pinNote={() => {}}
-            editNote={() => {}}
-            deleteNote={() => {}}
-          />
-          <NoteCard
-            title="Plan Weekend Getaway"
-            date="Jun 5, 2025"
-            content="Start planning a weekend getaway trip to Monterey."
-            tags="#Travel"
-            isPinned={false}
-            pinNote={() => {}}
-            editNote={() => {}}
-            deleteNote={() => {}}
-          />
+          {allNotes.map((note, index) => (
+            <NoteCard
+              key={note._id}
+              title={note.title}
+              date={new Date(note.createdOn).toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}
+              content={note.content}
+              tags={note.tags}
+              isPinned={note.isPinned}
+              pinNote={() => {}}
+              editNote={() => {}}
+              deleteNote={() => {}}
+            />
+          ))}
         </div>
       </div>
       <button className="w-12 h-12 flex items-center justify-center rounded-full cursor-pointer bg-blue-500 hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {setAddEditModal({show: true, type: "add", data: null})}}>
