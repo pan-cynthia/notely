897e36759366efa2b7de9f94158923211b039c2b
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-21 14:05:20 -0700

    remove test message logs

--- frontend/src/utils/authentication.js
+++ frontend/src/utils/authentication.js
@@ -5,8 +5,6 @@ export const isTokenValid = (accessToken) => {
   try {
     const decoded = jwtDecode(accessToken);
     const currTime = Date.now() / 1000; // seconds
-    console.log("Expiration: ", decoded.exp);
-    console.log("Now: ", Date.now());
     return decoded.exp > currTime;
   } catch (error) {
     // token is expired, user needs to login again
