eeb02f9e9fb32535af1ad793c396d60613b16bd3
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-21 14:04:36 -0700

    set main content area to root element, hide from screen reader when modal is open

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Modal from 'react-modal';
 import './styles/index.css';
 import App from './App.jsx';
+
+Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
