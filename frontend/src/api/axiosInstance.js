f3b33753126b9030e48d775482599bea6076995b
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-28 16:48:04 -0700

    rename axios.js to axiosInstance.js for clarity

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  headers: {'Content-Type': 'application/json'}
+  baseURL: "http://localhost:3000/api",
+  headers: { "Content-Type": "application/json" },
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('accessToken');
+    const token = localStorage.getItem("accessToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
