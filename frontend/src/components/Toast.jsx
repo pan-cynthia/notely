c8950634adab97128661df7c3ec3a5bb34d9a8e6
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-28 12:49:23 -0700

    add animation to toast messages

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -1,19 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { LuCheck } from 'react-icons/lu';
 import { MdDeleteOutline } from 'react-icons/md';
 
 const Toast = ({ isShown, type, message, onClose }) => {
+  const [fadeOut, setFadeOut] = useState(false);
+
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      onClose();
-    }, 3000);
-    return () => {
-      clearTimeout(timeoutId);
+    if (isShown) {
+      setFadeOut(false);
+      const timer = setTimeout(() => {
+        setFadeOut(true);
+      }, 2500);
+      return () => {
+        clearTimeout(timer);
+      }
     }
-  }, [onClose]);
+  }, [isShown]);
+
+  useEffect(() => {
+    if (fadeOut) { // wait for animation to complete before calling onClose
+      const timer = setTimeout(() => {
+        onClose();
+      }, 500);
+      return () => {
+        clearTimeout(timer);
+      }
+    }
+  }, [fadeOut, onClose]);
 
   return (
-    <div className={`absolute top-20 right-6 ${isShown ? "opacity-100" : "opacity-0 -z-5"}`}>
+    <div className={`fixed top-20 right-6 transition-all duration-500 ease-in-out ${(isShown && !fadeOut) ? "opacity-100 z-50 translate-y-0" : "opacity-0 -z-5 translate-y-4"}`}> {/* set z-index to neg value, otherwise notecard in same spot cannot be pinned */}
       <div className={`min-w-52 bg-white rounded-md shadow-2xl before:w-[5px] before:h-full before:absolute before:left-0 before:top-0 before:rounded-l-lg ${type === "delete" ? "before:bg-red-500" : "before:bg-green-500"}`}>
         <div className="flex items-center gap-3 px-4 py-2">
           <div className={`w-10 h-10 flex items-center justify-center rounded-full ${type === "delete" ? "bg-red-50" : "bg-green-50"}`}> {/* circle background for check*/}
