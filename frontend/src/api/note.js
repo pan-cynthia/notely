af31edf3ccf62a81d6074fa67181a14093cfeed1
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-29 10:59:58 -0700

    centralized axios note API calls into one file

---
+++ frontend/src/api/note.js
@@ -0,0 +1,37 @@
+import axiosInstance from "./axiosInstance";
+
+export const addNote = (note) => {
+  return axiosInstance.post("/notes/add-note", {
+    title: note.title,
+    content: note.content,
+    tags: note.tags,
+  });
+};
+
+export const editNote = (note) => {
+  return axiosInstance.put(`/note/edit-note${note._id}`, {
+    title: note.title,
+    content: note.content,
+    tags: note.tags,
+  });
+};
+
+export const pinNote = (noteId, isPinned) => {
+  return axiosInstance.put(`/notes/pin-unpin-note/${noteId}`, {
+    isPinned: !isPinned,
+  });
+};
+
+export const deleteNote = (noteId) => {
+  return axiosInstance.delete(`/notes/delete-note/${noteId}`);
+};
+
+export const getAllNotes = () => {
+  return axiosInstance.get("/notes/get-all-notes");
+};
+
+export const searchNotes = (query) => {
+  return axiosInstance.get("/notes/search-notes", {
+    params: { query },
+  });
+};
