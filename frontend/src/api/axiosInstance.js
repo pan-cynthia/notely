f38f4640f944f059a20a7af9951166816062d542
Cynthia Pan <cynthiapan11@gmail.com>
2025-07-31 17:53:09 -0700

    use vite dev proxy to make all calls from same origin, avoid cross-origin requests

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { isTokenValid } from "../utils/authentication";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "/api",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
